d23b97822163d3426e702f7461cefe1766eb5bfb
Restiii <restisanchez99@gmail.com>
2022-03-29 11:49:11 +0200

    Update OMV_script.sh

--- OMV_script.sh
+++ OMV_script.sh
@@ -2,6 +2,9 @@
 # Script instalacion OpenMediaVault #
 #####################################
 
+# Ver los pasos
+set -x
+
 # Actualizamos repositorios
 
 apt update 
@@ -11,7 +14,20 @@ apt upgrade -y
 
 adduser pi ssh
 
-# Ejecutamos script instalacion OMV
+# Instalamos git
+apt install git
+
+# Clonamos el repositorio para la instalacion posterior de OpenVPN
+git clone https://github.com/RestiSanchez/Proyecto.git
+
+# Activamos y ejecutamos el ssh con:
+
+sudo systemctl enable ssh
+
+sudo systemctl start ssh
+
+
+# Descargamos y ejecutamos script instalacion OMV
 
 wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
 
